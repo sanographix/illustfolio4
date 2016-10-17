25b7e568bdf81279ad69a073e39204ea0981a958
sanographix <mail@sanographix.net>
2016-10-17 23:04:45 +0900

    タグリストしかjQuery使ってないので分離する

--- js/scripts-core.js
+++ js/scripts-core.js
@@ -7,25 +7,3 @@
     }
   }
 }());
-
-$(document).ready(function(){
-
-  // タグリスト記事出す
-  (function() {
-    var posts = new TumblrPosts({
-      // ドメインと取得する最大記事数
-      domain: location.host,
-      maxNum:300
-    });
-    posts.bind(posts.EVENT_COMPLETE, function(e) {
-      var that = this;
-      // getTags() でタグリストを取得
-      $.each(this.getTags(), function(i, tag) {
-        var html = '<li class="level' + (tag.count % 6 + 1) + '">'
-        + '<a href="/tagged/' + tag.name + '">' + tag.name + '</a></li>';
-        $(html).appendTo($("ul#tags"));
-      });
-    });
-    posts.run();
-  })();
-});
